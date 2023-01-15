93293d50ab658d0032db1280d91ff0f99ea176b0
Bolec <Bolec@DESKTOP-KA0JV61>
2023-01-15 20:20:55 +0100

    Add project files.

---
+++ Tydzien_2_Lekcja_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien_2_Lekcja_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
